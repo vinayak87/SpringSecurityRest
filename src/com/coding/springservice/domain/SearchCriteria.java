8bea37a6ab30189bd6ed1ee9a919b08eed685a60
vinayak87 <vvanvekar@gmail.com>
2016-02-10 04:14:37 +0530

    Create SearchCriteria.java

---
+++ src/com/coding/springservice/domain/SearchCriteria.java
@@ -0,0 +1,22 @@
+package com.coding.springservice.domain;
+
+import java.util.List;
+
+public class SearchCriteria {
+	
+	private String [] searchText;
+
+	public String[] getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String[] searchText) {
+		this.searchText = searchText;
+	}
+
+	
+	
+		
+		
+}
+
