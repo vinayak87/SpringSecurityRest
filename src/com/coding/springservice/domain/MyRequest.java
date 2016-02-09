800aabed5aa0321ef35d43f011bc5f9e7c0d288b
vinayak87 <vvanvekar@gmail.com>
2016-02-10 04:13:25 +0530

    Create MyRequest.java

---
+++ src/com/coding/springservice/domain/MyRequest.java
@@ -0,0 +1,14 @@
+package com.coding.springservice.domain;
+
+public class MyRequest {
+  String [] values ;
+
+public String[] getValues() {
+	return values;
+}
+
+public void setValues(String[] values) {
+	this.values = values;
+}
+  
+}
