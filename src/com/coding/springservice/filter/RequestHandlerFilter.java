1ad6948280307dcc89eec807ca2fd4def85121f3
vinayak87 <vvanvekar@gmail.com>
2016-02-10 04:15:31 +0530

    Create RequestHandlerFilter.java

---
+++ src/com/coding/springservice/filter/RequestHandlerFilter.java
@@ -0,0 +1,39 @@
+package com.coding.springservice.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestHandlerFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		String uri = ((HttpServletRequest) request).getRequestURI();
+		if (uri.endsWith(".jsp")||uri.endsWith(".js") ) {
+		    chain.doFilter(request, response); // Just let it go (assuming that files are in real not placed in a /spring folder!)
+		} else if (uri.endsWith("search")){
+		    request.getRequestDispatcher("/counter-api/search").forward(request, response); // Pass to Spring dispatcher servlet.
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
